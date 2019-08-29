5fe613327adfe3758683008cdf32000f255634df
Stella <stella.lan.yang@gmail.com>
2019-08-29 13:08:22 -0400

    tmp

---
+++ fff_ios/FBLogin/FBLoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  FBLoginViewController.swift
+//  FF
+//
+//  Created by Stella Yang on 8/29/19.
+//  Copyright © 2019 Jing Lin. All rights reserved.
+//
+
+import SwiftUI
+import FacebookLogin
+
+struct FBLoginViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FBLoginViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        FBLoginViewController()
+    }
+}
