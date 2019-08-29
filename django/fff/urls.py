7d77c86e5b3fb8f23257b80c53593b91df533ce8
ed1d1a8d <tong.t.wang@gmail.com>
2019-08-29 01:33:11 -0400

    Add auth flow

--- django/fff/urls.py
+++ django/fff/urls.py
@@ -18,5 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("auth/", include("rest_auth.urls")),
+    path("auth/registration/", include('rest_auth.registration.urls')),
     path("users/", include('users.urls')),
 ]
