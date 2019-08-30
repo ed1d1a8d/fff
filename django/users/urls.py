c0e60afa7d9b3b4e067248da8d4a1f10785d96d0
Yang Yan <yangawesome@gmail.com>
2019-08-30 02:45:03 -0400

    more friends endpoints

--- django/users/urls.py
+++ django/users/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("self", views.SelfDetail.as_view()),
     path("friends", views.FriendList.as_view()),
-    path("friends/<str:action>/<int:pk>",
-         views.FriendRequestsAction.as_view()),
+    path("friends/requests/<str:actions>", views.FriendsRequests.as_view()),
+    path("friends/actions/<str:action>/<int:pk>",
+         views.FriendsActions.as_view()),
 ]
