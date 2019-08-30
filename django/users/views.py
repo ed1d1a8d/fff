9c618ced9855faf52a14ce1c566e24de9ab3d8ce
Yang Yan <yangawesome@gmail.com>
2019-08-29 21:41:50 -0400

    .

--- django/users/views.py
+++ django/users/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpRequest
-from friendship.models import Friend
+from django.http import HttpResponse, JsonResponse
 from rest_framework import generics
 from rest_framework import permissions
+
+from friendship.models import Friend
+from friendship.models import FriendshipRequest
 
 from .models import User
 from .serializers import UserSerializer
@@ -21,14 +23,32 @@ class FriendList(generics.ListAPIView):
         return Friend.objects.friends(self.request.user)
 
 
-# details about a specific user who is a friend
-class FriendDetail(generics.RetrieveAPIView):
-   serializer_class = UserSerializer
+# friend requests
+class FriendRequestsAction(generics.GenericAPIView):
+    # return a response depending on the action passed in
+    def get(self, request, action, pk):
+        try:
+            other_user = User.objects.get(pk=pk)
+        except:
+            return HttpResponse("User pk does not exist.", status=400)
 
-   def get_object(self):
-       pk = self.kwargs["pk"]
-       other = User.objects.get(pk=pk)
-       print("pk", pk)
-       if Friend.objects.are_friends(self.request.user, other) != True:
-           return other # should be an error
-       return other
+        if action == "info":
+            if Friend.objects.are_friends(request.user, other_user) != True:
+                return HttpResponse("Target user is not a friend.", status=400)
+            serializer = UserSerializer([other_user], many=True)
+            return JsonResponse(serializer.data, safe=False)
+        elif action == "request":
+            try:
+                Friend.objects.add_friend(
+                    request.user,
+                    other_user)
+                return HttpResponse("Sent friendship request to pk " + other_user.id)
+            except Exception as exception:
+                return HttpResponse(str(exception), status=400)
+        elif action == "accept":
+            friend_request = FriendshipRequest.objects.get(to_user=request.user.id)
+            friend_request.accept()
+        elif action == "decline":
+            pass
+
+        return HttpResponse("Invalid action.", status=400)
