7dafa7e520d6c482fbcc3229393314f1261226c1
Yang Yan <yangawesome@gmail.com>
2019-08-30 02:51:08 -0400

    typo

--- deploy/ci-cd.sh
+++ deploy/ci-cd.sh
@@ -1,1 +1,1 @@
-while true; do git fetch; git reset --hard origin/production; python ../django/manage.py createstatic --no-input; python ../django/manage.py makemigrations; python ../django/manage.py migrate; sleep 30; done
+while true; do git fetch; git reset --hard origin/production; python ../django/manage.py collectstatic --no-input; python ../django/manage.py makemigrations; python ../django/manage.py migrate; sleep 30; done
