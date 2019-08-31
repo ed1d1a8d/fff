c0d3df9cba02e4209c9619aa549af3c7860df609
Jing Lin <moderatelyfunctionalprogrammer@gmail.com>
2019-08-31 01:11:08 -0400

    connected LobbyVC in ///

---
+++ fff_ios/FF/OptionsViewController/OptionsShowVCDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  FF
+//
+//  Created by Jing Lin on 8/31/19.
+//  Copyright © 2019 Jing Lin. All rights reserved.
+//
+
+protocol OptionsShowVCDelegate {
+    
+    func showVC(detailType: String)
+    
+}
