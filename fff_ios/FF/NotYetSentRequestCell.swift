1acfa6cd7feff1a3caf25e6b3ca258233fc1a1ae
Stella <stella.lan.yang@gmail.com>
2019-08-31 14:29:58 -0400

    Basic not yet sent cell

--- fff_ios/FF/NotYetSentRequestCell.swift
+++ fff_ios/FF/NotYetSentRequestCell.swift
@@ -1,5 +1,5 @@
 //
-//  OutgoingRequestCell.swift
+//  NotYetSentRequestCell.swift
 //  FF
 //
 //  Created by Stella Yang on 8/31/19.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class OutgoingRequestCell: UITableViewCell {
+class NotYetSentRequestCell: UITableViewCell {
     
     
     let container = FView(baseColor: UIColor.white)
@@ -19,18 +19,11 @@ class OutgoingRequestCell: UITableViewCell {
                            font: UIFont.systemFont(ofSize: 14.0),
                            color: UIColor.black)
     
-    let messageLabel = FLabel(text: "",
-                           font: UIFont.systemFont(ofSize: 14.0),
-                           color: UIColor.black)
-    
     let fakeProfPic = FImageView(name: "fakeProfPic",
                                  height: Dimensions.height * 0.055)
     
-    let blackX = FImageView(name: "blackX",
-                                 height: Dimensions.height * 0.022)
-    
     init(data: EatRequestData) {
-        super.init(style: .default, reuseIdentifier: "OutgoingRequestCell")
+        super.init(style: .default, reuseIdentifier: "NotYetSentRequestCell")
 
         self.translatesAutoresizingMaskIntoConstraints = false
         
@@ -42,16 +35,13 @@ class OutgoingRequestCell: UITableViewCell {
         
         self.nameLabel.text = data.friendName
         self.distLabel.text = String(format: "%.2fm", distance)
-        self.messageLabel.text = data.message
         
         self.container.layer.cornerRadius = 10
         self.container.clipsToBounds = true
         
         self.container.addSubview(self.fakeProfPic)
-        self.container.addSubview(self.blackX)
         self.container.addSubview(self.nameLabel)
         self.container.addSubview(self.distLabel)
-        self.container.addSubview(self.messageLabel)
         self.contentView.addSubview(self.container)
         
         addConstraints()
@@ -62,17 +52,14 @@ class OutgoingRequestCell: UITableViewCell {
     }
     
     func addConstraints() {
-        
-        self.container.addConstraints(FConstraint.paddingPositionConstraints(view: self.blackX, sides: [.right, .top], padding: 20))
-        
+                
         self.container.addConstraints(FConstraint.paddingPositionConstraints(view: self.fakeProfPic, sides: [.left], padding: 20))
-        
+
+        self.container.addConstraints(FConstraint.paddingPositionConstraints(view: self.fakeProfPic, sides: [.top], padding: 15))
+
         self.container.addConstraints(FConstraint.paddingPositionConstraints(view: self.nameLabel, sides: [.top], padding: 20))
         
         self.container.addConstraint(FConstraint.horizontalSpacingConstraint(leftView: self.fakeProfPic, rightView: self.nameLabel, spacing: 15))
-        
-        self.container.addConstraint(FConstraint.verticalSpacingConstraint(upperView: self.fakeProfPic, lowerView: self.messageLabel, spacing: 10))
-        
         
         self.container.addConstraints(FConstraint.paddingPositionConstraints(view: self.nameLabel, sides: [.top], padding: 20))
         self.container.addConstraint(FConstraint.fillXConstraints(view: self.nameLabel, widthRatio: 0.8))
@@ -81,14 +68,8 @@ class OutgoingRequestCell: UITableViewCell {
         self.container.addConstraint(FConstraint.verticalSpacingConstraint(upperView: self.nameLabel, lowerView: self.distLabel, spacing: 3))
         self.container.addConstraint(FConstraint.horizontalSpacingConstraint(leftView: self.fakeProfPic, rightView: self.distLabel, spacing: 15))
 
-        
-        self.container.addConstraint(FConstraint.verticalSpacingConstraint(upperView: self.distLabel, lowerView: self.messageLabel, spacing: 10))
-
-        self.container.addConstraints(FConstraint.paddingPositionConstraints(view: self.messageLabel, sides: [.left], padding: 15))
-
-        self.container.addConstraints(FConstraint.paddingPositionConstraints(view: self.messageLabel, sides: [.bottom], padding: 15))
+        self.container.addConstraints(FConstraint.paddingPositionConstraints(view: self.distLabel, sides: [.bottom], padding: 15))
     
-        
         self.contentView.addConstraints(FConstraint.paddingPositionConstraints(view: self.container, sides: [.left, .right], padding: 45))
         self.contentView.addConstraint(FConstraint.paddingPositionConstraint(view: self.container, side: .top, padding: 15))
         self.contentView.addConstraint(FConstraint.paddingPositionConstraint(view: self.container, side: .bottom, padding: 15))
