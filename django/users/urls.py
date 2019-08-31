33cd080b7e588e57525437a9406ebfdcaa3afdc1
ed1d1a8d <tong.t.wang@gmail.com>
2019-08-31 12:07:54 -0400

    Add basic Request model

--- django/users/urls.py
+++ django/users/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path("self/", views.SelfDetail.as_view()),
+    path("requests/create/", views.CreateRequest.as_view()),
+    path("requests/pending/incoming/",
+         views.PendingIncomingRequests.as_view()),
+    path("requests/pending/outgoing/",
+         views.PendingOutgoingRequests.as_view()),
     path("friends/", views.FriendList.as_view()),
     path("friends/requests/<str:actions>/", views.FriendsRequests.as_view()),
     path("friends/actions/<str:action>/<int:pk>/",
