57e09adffd6a1617d42ffcbabc9a66b3a54fdfd0
ed1d1a8d <tong.t.wang@gmail.com>
2019-08-31 16:23:20 -0400

    2 for real this time

--- django/users/urls.py
+++ django/users/urls.py
@@ -3,10 +3,13 @@ from . import views
 
 urlpatterns = [
     path("self/", views.SelfDetail.as_view()),
+    path("lobby-expiration/", views.LobbyExpiration.as_view()),
+    # Requests
     path("requests/create/", views.CreateRequest.as_view()),
     path("requests/incoming/<str:status>/", views.IncomingRequests.as_view()),
     path("requests/outgoing/<str:status>/", views.OutgoingRequests.as_view()),
     path("requests/respond/<int:pk>/<str:action>/", views.RespondToRequest.as_view()),
+    # Friends
     path("friends/", views.FriendList.as_view()),
     path("friends/requests/<str:actions>/", views.FriendsRequests.as_view()),
     path("friends/actions/<str:action>/<int:pk>/",
