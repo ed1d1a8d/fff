e87efdb54c724c7a9d780a1205a598cf005f4512
Yang Yan <yangawesome@gmail.com>
2019-08-30 21:39:07 -0400

    delete ds store, modify gitignore, and change up backend paths to be more organized

--- django/fff/urls.py
+++ django/fff/urls.py
@@ -20,17 +20,28 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic.base import RedirectView
 
-favicon_view = RedirectView.as_view(url="/static/favicon.ico", permanent=True)
-
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("auth/", include("rest_auth.urls")),
-    path("auth/registration/", include("rest_auth.registration.urls")),
-    path("users/", include("users.urls")),
 
-    path("dumb/", lambda request: HttpResponse("dumb")),
-    path("favicon.ico", favicon_view),
+    # registration has no trailing slash because we'll be using suffix patterns on it anyway
+    path("auth/registration", include("rest_auth.registration.urls")),
 ]
+urlpatterns = format_suffix_patterns(urlpatterns, allowed=["json"])
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# all the following urlpatterns end in /, since django will by default redirect
+# patterns to / if it's a 404
+urlpatterns += [
+    # test endpoint for sanity checking
+    path("dumb/", lambda request: HttpResponse("dumb")),
+
+    # default endpoint for admin actions
+    path("admin/", admin.site.urls),
+
+    # joke endpoint for the favicon which probably won't be used
+    path("favicon.ico/",
+         RedirectView.as_view(url="/static/favicon.ico", permanent=True)),
+
+    # auth'd endpoint for user stuff
+    path("users/", include("users.urls")),
+]
 urlpatterns += staticfiles_urlpatterns()
