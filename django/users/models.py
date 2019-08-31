416626d60cb1e72f1b9d9733e0a48c8c64a8b13b
ed1d1a8d <tong.t.wang@gmail.com>
2019-08-31 12:07:54 -0400

    Add basic Request model

--- django/users/models.py
+++ django/users/models.py
@@ -11,6 +11,20 @@ class User(AbstractUser):
     lon = models.FloatField(null=True, blank=True)
     lat = models.FloatField(null=True, blank=True)
 
-    # How long user will remain "online"
     # Always in utc
-    # online_until = models.DateTimeField(default=datetime.utcnow())
+    lobby_expiration = models.DateTimeField(default=datetime.utcnow())
+
+
+class Request(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    message = models.TextField(max_length=140)
+
+    # Pending, Accepted, Rejected, Expired, Cancelled
+    status = models.CharField(blank=False, max_length=255)
+
+    sender = models.ForeignKey(User,
+                               related_name="request_sender_set",
+                               on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User,
+                                 related_name="request_receiver_set",
+                                 on_delete=models.CASCADE)
--- django/users/serializers.py
+++ django/users/serializers.py
@@ -7,3 +7,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.User
         fields = ["id", "username", "name", "facebook_id", "lat", "lon"]
+
+
+class RequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Request
+        fields = ["created_at", "message", "status", "sender", "receiver"]
