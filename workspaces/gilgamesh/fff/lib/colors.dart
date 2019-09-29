2df7ebe72a6eeeb2bee94f36cc5a045b0feafbe1
Yang (Gilbert) Yan <~@gilgamesh.cc>
2019-09-29 15:46:19 -0400

    basic layout for add friends

---
+++ workspaces/gilgamesh/fff/lib/colors.dart
@@ -0,0 +1,8 @@
+library colors;
+
+import 'package:flutter/widgets.dart';
+
+final Color white = const Color(0xFFFFFFFF);
+final Color black = const Color(0xFF000000);
+final Color background = const Color(0xFFFFF5E1);
+final Color strongBackground = const Color(0xFFFFDA93);
