0f52c8359e16cb8e2364ed164ff21b5d842048d5
Stella <stella.lan.yang@gmail.com>
2019-10-05 00:52:46 -0400

    Add gradient box

--- fff_flutter/lib/utils/colors.dart
+++ fff_flutter/lib/utils/colors.dart
@@ -8,3 +8,6 @@ const Color background = const Color(0xFFFFF5E1);
 const Color strongBackground = const Color(0xFFFFDA93);
 const Color navBarBackground = const Color(0xFFFFF1D4);
 const Color lightGray = const Color(0xFF8A8A8A);
+
+const Color gradientTop = const Color(0xFFFFA800);
+const Color gradientBottom = const Color(0xFFE25A5A);
