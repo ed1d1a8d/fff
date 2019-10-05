1f4595e8c527fca5ed75fe35d40d683340d4e1d6
Stella <stella.lan.yang@gmail.com>
2019-10-05 12:48:17 -0400

    Fix overflow issues

--- fff_flutter/lib/views/home.dart
+++ fff_flutter/lib/views/home.dart
@@ -43,21 +43,73 @@ class _HomeState extends State<Home> {
         iconTheme: new IconThemeData(color: Colors.black),
       ),
       endDrawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
+        child: new Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             DrawerHeader(
-              child: Text('Drawer Header'),
-              decoration: BoxDecoration(
-                color: Colors.orange,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                      width: 75.0,
+                      height: 75.0,
+                      decoration: new BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: new DecorationImage(
+                            fit: BoxFit.fill,
+                            image: new NetworkImage(
+                                "https://scontent.fbed1-2.fna.fbcdn.net/v/t1.0-1/c73.206.614.614a/s100x100/50735968_2302236259809637_5312553092619698176_n.jpg?_nc_cat=104&_nc_oc=AQnj-x9fkxHyDUJPaGK_RlQ_kTJhJBbDXNx29xDpk5A_7NMlgR6Nv4Qg2VCquwC9vf8&_nc_ht=scontent.fbed1-2.fna&oh=165917455e36027c7672ef070ed06e8d&oe=5E3A0206"),
+                          ))),
+                  SizedBox(
+                    height: 14,
+                  ),
+                  Text(
+                    "Stella Yang",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 3,
+                  ),
+                  Text("@stellay")
+                ],
               ),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: BorderDirectional(
+                      bottom: BorderSide(
+                    color: Colors.orange,
+                  ))),
             ),
             ListTile(
-              title: Text('Item 1'),
+              title: Row(
+                children: <Widget>[
+                  SizedBox(width: 20),
+                  Icon(FontAwesomeIcons.home, size: 16),
+                  SizedBox(width: 20),
+                  Text("Home"),
+                ],
+              ),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+                Navigator.pop(context);
+                setState(() {
+                  _currentIndex = 1;
+                });
+              },
+            ),
+            ListTile(
+              title: Row(
+                children: <Widget>[
+                  SizedBox(width: 20),
+                  Icon(FontAwesomeIcons.search, size: 16),
+                  SizedBox(width: 20),
+                  Text("Search People"),
+                ],
+              ),
               onTap: () {
                 // Update the state of the app.
                 // ...
@@ -65,12 +117,56 @@ class _HomeState extends State<Home> {
               },
             ),
             ListTile(
-              title: Text('Item 2'),
+              title: Row(
+                children: <Widget>[
+                  SizedBox(width: 20),
+                  Icon(FontAwesomeIcons.smile, size: 16),
+                  SizedBox(width: 21),
+                  Text("Friend Requests"),
+                ],
+              ),
               onTap: () {
                 // Update the state of the app.
                 // ...
                 Navigator.pop(context);
               },
+            ),
+            ListTile(
+              title: Row(
+                children: <Widget>[
+                  SizedBox(width: 20),
+                  Icon(FontAwesomeIcons.questionCircle, size: 16),
+                  SizedBox(width: 20),
+                  Text("Help"),
+                ],
+              ),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+                Navigator.pop(context);
+              },
+            ),
+            new Expanded(
+              child: new Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 6),
+                    child: ListTile(
+                      title: Row(
+                        children: <Widget>[
+                          SizedBox(width: 20),
+                          Icon(FontAwesomeIcons.powerOff, size: 16),
+                          SizedBox(width: 20),
+                          Text("Log Out"),
+                        ],
+                      ),
+                      onTap: () {
+                        // Update the state of the app.
+                        // ...
+                        Navigator.pop(context);
+                      },
+                    ),
+                  )),
             ),
           ],
         ),
@@ -82,12 +178,10 @@ class _HomeState extends State<Home> {
               canvasColor: fff_colors.navBarBackground,
               // sets the active color of the `BottomNavigationBar` if `Brightness` is light
               primaryColor: Colors.orange,
-              textTheme: Theme.of(context)
-                  .textTheme
-                  .copyWith(caption: new TextStyle(color: fff_colors.lightGray))),
+              textTheme: Theme.of(context).textTheme.copyWith(
+                  caption: new TextStyle(color: fff_colors.lightGray))),
           // sets the inactive color of the `BottomNavigationBar`
           child: SizedBox(
-            height: 64,
             child: BottomNavigationBar(
               onTap: onTabTapped,
               currentIndex: _currentIndex,
