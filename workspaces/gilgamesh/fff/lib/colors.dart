6e8904c28fb52e69b82a8c3b2586d34641438e0e
Yang (Gilbert) Yan <~@gilgamesh.cc>
2019-10-13 01:19:01 -0400

    remove irrelevant workspaces

--- workspaces/gilgamesh/fff/lib/colors.dart
+++
@@ -1,8 +0,0 @@
-library colors;
-
-import 'package:flutter/widgets.dart';
-
-const Color white = const Color(0xFFFFFFFF);
-const Color black = const Color(0xFF000000);
-const Color background = const Color(0xFFFFF5E1);
-const Color strongBackground = const Color(0xFFFFDA93);
