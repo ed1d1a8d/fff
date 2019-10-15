03361047f49100fbefe222b4f0dc72f3bfb3718a
Jing Lin <moderatelyfunctionalprogrammer@gmail.com>
2019-10-15 16:34:45 -0400

    integrating Google Maps

--- fff_flutter/lib/main.dart
+++ fff_flutter/lib/main.dart
@@ -7,21 +7,30 @@ import "package:fff/views/home.dart";
 import "package:fff/views/loading.dart";
 import "package:fff/views/login.dart";
 import "package:flutter/material.dart";
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(FFFApp());
 
 class FFFApp extends StatelessWidget {
+
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
 
       // Since we don't have animations between view widgets yet, to test, just switch this line out.
-      home: Home(),
 //      home: Loading(),
       // home: Login(),
       // home: AddFriendsSignup(),
-      // home: Home(),
+       home: Home(),
       // home: FriendRequests(),
 
       // Used by the navigator.
