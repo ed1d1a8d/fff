4aba49f8bd3be864238d66d6609a934e816cd62f
Stella <stella.lan.yang@gmail.com>
2019-10-19 17:49:49 -0400

    frontend integration with ffrequests

--- fff_flutter/lib/backend/lobby.dart
+++ fff_flutter/lib/backend/lobby.dart
@@ -1,5 +1,6 @@
 import "package:fff/backend/auth.dart" as fff_auth;
 import "package:fff/backend/constants.dart" as fff_backend_constants;
+import 'package:fff/models/ffrequest.dart';
 import 'package:fff/models/mock_data.dart';
 import "package:fff/models/user_data.dart";
 import "package:http/http.dart" as http;
@@ -19,4 +20,16 @@ Future<List<UserData>> fetchOnlineFriends() async {
     throw new Exception("Failed to get lobby friends...");
 
   return UserData.listFromJsonString(response.body);
+}
+
+Future<List<FFRequest>> fetchIncomingRequests() async {
+  if (fff_backend_constants.mockData) {
+    return MockData.incomingRequests;
+  }
+}
+
+Future<List<FFRequest>> fetchOutgoingRequests() async {
+  if (fff_backend_constants.mockData) {
+    return MockData.outgoingRequests;
+  }
 }
