577a7ccebb9ce31d92114f1324e6bc5b7feffef7
ed1d1a8d <tong.t.wang@gmail.com>
2019-10-29 18:41:17 -0400

    Fix emoji

--- fff_flutter/lib/main.dart
+++ fff_flutter/lib/main.dart
@@ -48,8 +48,8 @@ class _FFFAppState extends State<FFFApp> {
       theme: theme,
 
       // Switch this line out for easier debugging
-      // home: Loading(),
-      home: Login(),
+      home: Loading(),
+      // home: Login(),
       // home: AddFriendsSignup(),
       // home: Home(),
       // home: SearchPeople(),
