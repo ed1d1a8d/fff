52bc1a583d24c39559846db7d2b444f731a25752
Yang Yan <~@gilgamesh.cc>
2019-11-03 17:52:54 -0500

    use mus.icu backend

--- fff_flutter/lib/backend/constants.dart
+++ fff_flutter/lib/backend/constants.dart
@@ -1,7 +1,8 @@
 const bool localMockData = false;
 const bool remoteMockData = true;
-const String server_location = "http://10.0.2.2:8000";
+// const String server_location = "http://10.0.2.2:8000";
 // const String server_location = "http://localhost:8000";
+const String server_location = "https://mus.icu";
 
 // to tunnel, run ssh -R fff:80:127.0.0.1:8000 serveo.net in a terminal
 //const String server_location = "https://fff.serveo.net";
