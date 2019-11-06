83effa6c14143f9899fa7e81f5e2f3640e6953f6
Yang Yan <yangawesome@gmail.com>
2019-11-06 10:01:55 -0500

    backend to mus.icu and add launch config from vscode

--- fff_flutter/lib/backend/constants.dart
+++ fff_flutter/lib/backend/constants.dart
@@ -1,8 +1,8 @@
 const bool localMockData = false;
 const bool remoteMockData = false;
-//const String server_location = "http://10.0.2.2:8000";
-const String server_location = "http://localhost:8000";
-// const String server_location = "https://mus.icu";
+// const String server_location = "http://10.0.2.2:8000";
+// const String server_location = "http://localhost:8000";
+const String server_location = "https://mus.icu";
 
 // to tunnel, run ssh -R fff:80:127.0.0.1:8000 serveo.net in a terminal
 // const String server_location = "https://fff.serveo.net";
